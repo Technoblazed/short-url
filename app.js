4862e596790404ace60257cc5bb24fe2aea9200f
Technoblazed <anthonyjohnsonadams@gmail.com>
2018-08-14 19:00:56 +0100

    Add clear endpoint

--- app.js
+++ app.js
@@ -1,98 +1,85 @@
-const _ = require('lodash');
-const db = require('./models');
 const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
 
 const Koa = require('koa');
+const db = require('./models');
+
 const app = new Koa();
 
 app.use(bodyParser());
 app.use(logger());
 
 app.use(async(ctx) => {
-  const method = ctx.request.method;
-  const url = ctx.request.url;
+  const { method, url } = ctx.request;
+  const { redirectTarget, redirectToken } = ctx.request.body;
   const token = url.replace(/\/\//g, '/').split('/')[1].split('?')[0];
 
   if (method === 'GET' && token) {
-    const result = await db.shorturl.findOne({
-      where: {
-        redirectToken: token
-      }
-    });
+    const result = await db.shorturl.findOne({ where: { redirectToken: token } });
 
     if (result) {
       return ctx.redirect(`${/(http(s?)):\/\//i.test(result.redirectTarget) ? '' : 'http://'}${result.redirectTarget}`);
-    } else {
-      ctx.body = {
-        status: 404,
-        message: 'Not Found'
-      };
     }
-  } else if (method === 'POST' && token === 'new') {
-    const redirectTarget = ctx.request.body.redirectTarget;
-    const redirectToken = ctx.request.body.redirectToken;
 
+    ctx.body = { status: 404, message: 'Not Found' };
+  } else if (method === 'POST' && token === 'new') {
     if (redirectTarget) {
       try {
         if (redirectToken) {
           const [result, created] = await db.shorturl.findOrCreate({
-            where: {
-              redirectToken
-            },
-            defaults: {
-              redirectTarget,
-              redirectToken
-            }
+            where: { redirectToken },
+            defaults: { redirectTarget, redirectToken }
           });
 
           if (created) {
             ctx.body = {
               status: 200,
               message: 'OK',
-              data: {
-                redirectTarget,
-                redirectToken: result.redirectToken
-              }
+              data: { redirectTarget, redirectToken: result.redirectToken }
             };
           } else {
-            ctx.body = {
-              status: 400,
-              message: 'Bad Request',
-              error: 'Token already exists'
-            };
+            ctx.body = { status: 400, message: 'Bad Request', error: 'Token already exists' };
           }
         } else {
-          const result = await db.shorturl.create({
-            redirectTarget
-          });
+          const result = await db.shorturl.create({ redirectTarget });
 
           ctx.body = {
             status: 200,
             message: 'OK',
-            data: {
-              redirectTarget,
-              redirectToken: result.redirectToken
-            }
+            data: { redirectTarget, redirectToken: result.redirectToken }
           };
         }
       } catch (e) {
-        ctx.body = {
-          status: 400,
-          message: 'Bad Request',
-          error: 'Database query failed'
-        };
+        ctx.body = { status: 400, message: 'Bad Request', error: 'Database query failed' };
       }
     } else {
+      ctx.body = { status: 400, message: 'Bad Request', error: 'No redirectTarget specified' };
+    }
+  } else if (method === 'POST' && token === 'clear') {
+    try {
+      if (redirectToken) {
+        const affectedRows = await db.shorturl.destroy({ where: { redirectToken } });
+
+        if (affectedRows) {
+          ctx.body = { status: 200, message: 'OK', data: { redirectToken } };
+        } else {
+          ctx.body = { status: 400, message: 'Bad Request', error: 'Token does not exist' };
+        }
+      } else {
+        ctx.body = { status: 400, message: 'Bad Request', error: 'No redirectToken specified' };
+      }
+    } catch (e) {
       ctx.body = {
         status: 400,
         message: 'Bad Request',
-        error: 'No redirectTarget specified'
+        error: 'Database query failed'
       };
     }
   } else {
-    ctx.redirect('http://mythicleague.com');
+    return ctx.redirect('http://mythicleague.com');
   }
+
+  return true;
 });
 
 app.listen(3000, () => {
--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const config = require('../config');
 const Sequelize = require('sequelize');
+const config = require('../config');
+
 const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
   host: config.db.host,
   dialect: config.db.dialect,
@@ -32,9 +33,7 @@ const sequelize = new Sequelize(config.db.database, config.db.username, config.d
 
 const db = {};
 
-fs.readdirSync(__dirname).filter((file) => {
-  return file.indexOf('.') !== 0 && file !== 'index.js';
-}).forEach((file) => {
+fs.readdirSync(__dirname).filter(file => file.indexOf('.') !== 0 && file !== 'index.js').forEach((file) => {
   const model = sequelize.import(path.join(__dirname, file));
 
   db[model.name] = model;
